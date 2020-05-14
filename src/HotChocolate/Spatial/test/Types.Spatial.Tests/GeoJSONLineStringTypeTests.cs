50722fe68463c6b144dc0651806704fa88797a5c
steveoh <sgourley@utah.gov>
2020-05-14 17:50:58 -0600

    wip(spatial): start output type tests

---
+++ src/HotChocolate/Spatial/test/Types.Spatial.Tests/GeoJSONLineStringTypeTests.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using HotChocolate.Execution;
+using HotChocolate.Language;
+using NetTopologySuite.Geometries;
+using Snapshooter.Xunit;
+using Xunit;
+
+namespace HotChocolate.Types.Spatial.Tests
+{
+    public class GeoJSONLineStringTypeTests
+    {
+        [Fact]
+        public void What_do_we_test_with_output_types()
+        {
+            Assert.True(false);
+        }
+    }
+}
